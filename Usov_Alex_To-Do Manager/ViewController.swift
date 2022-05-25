fb6655106dcefdf89966b526c29c216c0aad9087
Alex <alexmihgel@gmail.com>
2022-05-25 11:24:41 +0300

    Initial Commit

---
+++ Usov_Alex_To-Do Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Usov_Alex_To-Do Manager
+//
+//  Created by Алексей Попроцкий on 25.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
